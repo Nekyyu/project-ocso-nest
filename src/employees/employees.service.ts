fdda1162f018729e42c9c17b29790e55f673d1d0
Nekyyu <josephaleons@gmail.com>
2024-09-14 21:14:48 -0600

    Database config

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,17 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import {v4 as uuid} from "uuid";
 
 @Injectable()
 export class EmployeesService {
   private employees: CreateEmployeeDto[]=[{
-    id: 1,
+    id: uuid(),
     name: "Alberto",
     lastName: "Costas",
     phoneNumber: "XXX43221"
   },
   {
-    id: 2,
+    id: uuid(),
     name: "José",
     lastName: "Pérez",
     phoneNumber: "4424213XX"
@@ -19,7 +20,7 @@ export class EmployeesService {
   ]
 
   create(createEmployeeDto: CreateEmployeeDto) {
-    createEmployeeDto.id = this.employees.length+1
+    createEmployeeDto.id = uuid()
     this.employees.push(createEmployeeDto);
     return this.employees;
   }
@@ -29,27 +30,31 @@ findAll() {
     return this.employees;
   }
 
-  findOne(id: number) {
+   findOne(id: string) {
     const employee = this.employees.filter((employee)=>employee.id === id)[0]
+    if (!employee) throw new NotFoundException();
     return employee;
   }
 
-  update(id: number, updateEmployeeDto: UpdateEmployeeDto) {
+  update(id: string, updateEmployeeDto: UpdateEmployeeDto) {
     let employeeToUpdate = this.findOne(id);
     employeeToUpdate = {
       ...employeeToUpdate,
       ...updateEmployeeDto
     }
+    if (employeeToUpdate) throw new NotFoundException();
     this.employees = this.employees.map((employee)=>{
       if (employee.id === id) {
           employee = employeeToUpdate
       }
       return employee;
     })
+
     return employeeToUpdate;
   }
 
-  remove(id: number) {
+  remove(id: string) {
+   this.findOne(id);
    this.employees = this.employees.filter((employee)=>employee.id != id);
    return this.employees;
   }
